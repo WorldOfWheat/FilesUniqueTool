b98a455acce3a3fd49bf6ce8130c3b3de02d1b07
小麥 <86335529+WorldOfWheat@users.noreply.github.com>
2023-02-03 20:49:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from collections import defaultdict
+from sortedcontainers import SortedList
 import os
 
-def printInfo():
-    print('-----------------------------------------------------------')
-    print('歡迎使用去重工具 v1.0')
-    print('作者：小麥 WorldOfWheat')
-    print('GitHub： https://github.com/WorldOfWheat/FilesUniqueTool')
-    print('-----------------------------------------------------------')
+welcomeMsg = '''-----------------------------------------------------------
+歡迎使用去重工具 v1.0
+作者：小麥 WorldOfWheat
+GitHub： https://github.com/WorldOfWheat/FilesUniqueTool
+-----------------------------------------------------------'''
+
+successfulMsg = '''-----------------------------------------------------------
+執行完成
+-----------------------------------------------------------'''
 
 def getInput():
     result = [] 
@@ -21,7 +25,7 @@ def getInput():
     result.append(_input)
 
     while True:
-        _input = input('是否去重路徑內的所有資料夾 true/false: ')
+        _input = input('是否去重路徑內的所有資料夾內的檔案 true/false: ')
         if (_input.lower() == 'true') or (_input.lower() == 'false') or (_input == '1') or (_input == '0'):
             break
         print('輸入格式錯誤')
@@ -31,29 +35,29 @@ def getInput():
 
 def uniquePath(path, withDir):
     allFiles = os.listdir(path)
-    md = defaultdict(list)
+    _map = {}
     for i in allFiles:
         fullPath = path + '\\' + i
         if os.path.isdir(fullPath):
             if withDir:
                 uniquePath(fullPath, True)
             continue
-        fileBytes = open(fullPath, 'rb')
-        data = fileBytes.read(512)
-        fileBytes.close()
-        md[data.hex()].append(fullPath)
-    for i in md:
-        md[i].sort()
-        for j in md[i][:-1]:
+        with open(fullPath, 'rb') as file:
+            data = file.read(512)
+            if _map.get(data.hex(), None) == None:
+                _map[data.hex()] = SortedList()
+            _map[data.hex()].add(fullPath)
+    for i in _map:
+        for j in _map[i][:-1]:
+            print(j)
             os.remove(j)
 
 def main():
-    printInfo()
-    path = getInput()
-    uniquePath(path[0], path[1])
-    print('-------------------------去重完成--------------------------')
-    os.system('pause')
-    
+    print(welcomeMsg)
+    _input = getInput()
+    uniquePath(_input[0], _input[1])
+    print(successfulMsg)
 
 if __name__ == '__main__':
     main()
+    os.system('pause')
